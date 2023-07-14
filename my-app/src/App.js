7d4379fab2a1ba5a8f80955e9a70977266ea5f64
muhdshamil02 <shamilv02@gmail.com>
2023-07-14 05:46:13 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Child from './Component/Child';
+import Component1 from './Component/Component1';
+import Home from './Component/Home';
+import './Component/Style.css';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Home />
+      <Child />
+      <Component1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Component/Component1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Component1() {
+  return (
+    <div>
+        <h2>Hello..!</h2>
+    </div>
+  )
+}
+
+export default Component1
